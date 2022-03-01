811bb01daf606853b826d1c2cab568db269f0d7d
Roseweltty <roseweltty@visie.com.br>
2022-03-01 11:48:34 -0300

    chore: configuração para deploy heroku

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,8 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
 const app_1 = require("./app");
-const PORT = process.env.PORT || 3000;
-app_1.app.listen(PORT, () => {
-    console.log(`Aplicação rodando na porta ${PORT}`);
-});
+app_1.app.listen(process.env.PORT || 3333);
