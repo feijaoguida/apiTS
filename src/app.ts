826394c6f6e1e0f7283acdb76c243493b44710dc
Roseweltty <roseweltty@visie.com.br>
2022-03-01 09:16:42 -0300

    chore: instalação e config do CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { router } from './routes'
 import swaggerUi from 'swagger-ui-express'
+import cors from 'cors'
 
 const swaggerDocument = require('../swagger.json')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(
